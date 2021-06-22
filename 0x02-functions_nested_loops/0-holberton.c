aa62ffe9149df002de113719759e0e8052862b63
Anyim Ijeoma <thetechmavin@gmail.com>
2021-06-22 13:58:24 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Print the word "Holberton"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char p[] = "Holberton";
+for (i = 0; i < 9; i++)
+{
+putchar(p[i]);
+}
+putchar('\n');
+return (0);
+}
